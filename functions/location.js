327c70f1c6787be965233c86c043509280b760aa
Frank Gjertsen <frank.gjertsen@gmail.com>
2022-01-22 16:52:01 +0100

    Update wrangler@beta, change test function

--- functions/location.js
+++ functions/location.js
@@ -4,12 +4,16 @@ export async function onRequest({ request, env }) {
 	// const value2 = context.env.TEST.get("click");
 	// console.log({env});
 
-	const value = await env.KV.get("flags");
-	console.log(env, value);
+	// const value = await env.KV.get("flags");
+	// console.log(env, value);
 
-	await env.KV.put("flags", "test");
+	// await env.KV.put("flags", "test");
+	const task = await env.KV.get("click");
+  return new Response(task);
 
-	const { cf } = request;
-	return new Response(JSON.stringify({...cf, value}, null, 2), { 'Content-Type': 'application/json' });
+
+	// const { cf } = request;
+	// return new Response(JSON.stringify({...cf, value}, null, 2), { 'Content-Type': 'application/json' });
+	// return new Response(JSON.stringify({ test: 'hei', request, env }));
 	// return new Response('value');
 }
