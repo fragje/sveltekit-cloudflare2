09ba47f6e76e74778689cab152bc4920ffc31e4f
Frank Gjertsen <frank.gjertsen@gmail.com>
2022-01-28 11:57:36 +0100

    Change kv namespaces

--- functions/location.js
+++ functions/location.js
@@ -8,7 +8,7 @@ export async function onRequest({ request, env }) {
 	// console.log(env, value);
 
 	// await env.KV.put("flags", "test");
-	const task = await env.TodoList.get("todos");
+	const task = await env.TODO_LIST.get("todos");
 
   return new Response(task);
 
